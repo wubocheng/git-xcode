bdcd69ccde7145c49004a732c6d797ee502a60ba
吴伯程 <wubocheng@wubochengdeMacBook-Pro.local>
2015-07-01 10:58:58 +0800

    Initial Commit

---
+++ Git-github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git-github
+//
+//  Created by 吴伯程 on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Git-githubTests/Git_githubTests.m
@@ -0,0 +1,40 @@
+//
+//  Git_githubTests.m
+//  Git-githubTests
+//
+//  Created by 吴伯程 on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Git_githubTests : XCTestCase
+
+@end
+
+@implementation Git_githubTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
